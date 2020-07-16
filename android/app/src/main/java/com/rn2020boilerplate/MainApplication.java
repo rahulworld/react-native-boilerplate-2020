df1401c0ce908345fede797da08fd2cbd3c0d664
Rahul Chauhan <chauhanr371@gmail.com>
2020-07-16 17:36:37 +0530

    splash screen setup

--- android/app/src/main/java/com/rn2020boilerplate/MainApplication.java
+++ android/app/src/main/java/com/rn2020boilerplate/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import io.liteglue.SQLitePluginPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
@@ -11,6 +12,9 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+
+// react-native-splash-screen >= 0.3.1
+import org.devio.rn.splashscreen.SplashScreen; // here
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -27,6 +31,7 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
+          // packages.add(new SplashScreenReactPackage());
           return packages;
         }
 
